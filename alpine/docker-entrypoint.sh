b6494834ed3b2c171ccf4b85bfea3e6bcf793cd7
larkost-fetch <kkuehn@fetchrobotics.com>
2018-05-10 11:49:12 -0700

    Enable setting user and group via ENV variables

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -2,12 +2,14 @@
 set -e
 
 MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
+MITMPROXY_USER=${MITMPROXY_USER:-mitmproxy}
+MITMPROXY_GROUP=${MITMPROXY_GROUP:-mitmproxy}
 
 if [[ "$1" = "mitmdump" || "$1" = "mitmproxy" || "$1" = "mitmweb" ]]; then
         mkdir -p "$MITMPROXY_PATH"
-        chown -R mitmproxy:mitmproxy "$MITMPROXY_PATH"
+        chown -R $MITMPROXY_USER:$MITMPROXY_GROUP "$MITMPROXY_PATH"
 
-        su-exec mitmproxy "$@"
+        su-exec $MITMPROXY_USER "$@"
 else
-        exec "$@"
+        su-exec "$@"
 fi
